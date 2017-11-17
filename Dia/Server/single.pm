6b204b19f4c6322972dab9b07e8552197bbd4df7
Dmitry Ovsyanko <do_@pochta.ru>
2017-11-17 17:41:34 +0300

    Server/single: configuration loading fixed

--- Dia/Server/single.pm
+++ Dia/Server/single.pm
@@ -5,19 +5,22 @@ use IO::File;
 
 sub start {
 		
+	do 'Dia/Conf.pm';
+
 	require Dia::Loader;
-	Dia::Loader -> import ();
 	
+	APP::import ();
+	APP::require_config ();
+	APP::sql_reconnect  ();
+	APP::require_model  ();
+
 	my $f = $APP::preconf -> {fcgi};
 	
 	$f -> {address} ||= ':' . $f -> {port};
 	$f -> {backlog} ||= 1024;
 		
-	APP::sql_reconnect  ();
-	APP::require_model  ();		
-	
 	my $socket = FCGI::OpenSocket ($f -> {address}, $f -> {backlog});
-			
+
 	my $request = FCGI::Request (\*STDIN, \*STDOUT, new IO::File, \%ENV, $socket);
 
 	while ($request -> Accept >= 0) {
