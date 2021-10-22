104e9b0022199ead1327a6f4db22938c9e5484a4
Maksim Gruzdev <mgruzdev@cheby.ru>
2021-10-22 16:23:22 +0300

    recreate view FIX
    2BP01: cannot drop view fias because other objects depend on it

--- Dia/SQL/Dialect/PostgreSQL/Wish/views.pm
+++ Dia/SQL/Dialect/PostgreSQL/Wish/views.pm
@@ -6,7 +6,7 @@ sub wish_to_actually_create_views {
 	
 	foreach my $i (@$items) {
 
-		sql_do ("DROP VIEW IF EXISTS $i->{name}");
+		sql_do ("DROP VIEW IF EXISTS $i->{name} CASCADE");
 
 		sql_do ("CREATE VIEW $i->{name} ($i->{columns}) AS $i->{sql}");
 
